01fa3905b106112915d8e809f34d9a38993ac45e
Susmitha Selvakumar <susmithaselvakumar07l@gmail.com>
2026-07-18 19:21:14 -1100

    Fix Gemini API for Streamlit Cloud

--- ai/gemini_chat.py
+++ ai/gemini_chat.py
@@ -1,17 +1,27 @@
 import os
+import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 
-# Load API Key
+# Load local .env
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Try local .env first
+api_key = os.getenv("GOOGLE_API_KEY")
+
+# If not available, use Streamlit Cloud Secrets
+if not api_key:
+    api_key = st.secrets.get("GOOGLE_API_KEY")
+
+# Configure Gemini
+genai.configure(api_key=api_key)
 
 # Gemini Model
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 def ask_teachtwin(context, question):
+
     prompt = f"""
 You are TeachTwin AI.
 
@@ -24,7 +34,7 @@ Student Question:
 {question}
 
 If the answer is not found in the study material, reply:
-'Sorry, this topic is not available in your teacher's uploaded notes.'
+"Sorry, this topic is not available in your teacher's uploaded notes."
 """
 
     response = model.generate_content(prompt)
